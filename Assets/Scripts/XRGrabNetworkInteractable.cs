dee3c799b909dbb00473b61573d30e81f2c8d622
mrmclaughlin <tom@mrmclaughlin.com>
2023-10-19 13:06:09 -0400

    almost done

--- Assets/Scripts/XRGrabNetworkInteractable.cs
+++ Assets/Scripts/XRGrabNetworkInteractable.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using Photon.Pun;
+
+
 public class XRGrabNetworkInteractable : XRGrabInteractable
 {
+	
+	private Rigidbody rigidBody;
+    private XRBaseInteractor grabInteractor;
+    private Vector3 interactorVelocity;
+    private Vector3 interactorAngularVelocity;
+	//[Tooltip("The projectile that's created")]
+    //public GameObject projectilePrefab = null;
+
+    //[Tooltip("The point that the project is created")]
+    //public Transform startPoint = null;
+
+    [Tooltip("The speed at which the projectile is launched")]
+    public float launchSpeed = .0010f;
+	 private XRBaseInteractor currentInteractor;
+	
 	private PhotonView photonView;
     // Start is called before the first frame update
     void Start()
     {
       photonView = GetComponent<PhotonView>();
     }
+	
+	
+	protected override void Awake()
+    {
+        base.Awake();
+        rigidBody = GetComponent<Rigidbody>();
+    }
+	
+	
+	
 
     // Update is called once per frame
     void Update()
@@ -23,7 +50,33 @@ public class XRGrabNetworkInteractable : XRGrabInteractable
 	
 	protected override void OnSelectEntering(XRBaseInteractor interactor)
 	{
-		photonView.RequestOwnership();
+		
 		base.OnSelectEntering(interactor);
+		photonView.RequestOwnership();
+	currentInteractor = interactor;
+
+        // Get and log the forward direction of the hand that grabbed the object
+        Vector3 handForward = currentInteractor.transform.forward;
+        Debug.Log("Hand forward direction on grab: " + handForward);
 	}
+	
+	protected override void OnSelectExited(XRBaseInteractor interactor)
+    {
+        base.OnSelectExited(interactor);
+		currentInteractor = interactor;
+       Vector3 handForward = currentInteractor.transform.forward;
+       
+		GameObject obj = GameObject.Find(gameObject.name);
+		Rigidbody rb = obj.GetComponent<Rigidbody>();
+		Vector3 force = handForward * launchSpeed;
+        rb.AddForce(force);
+		
+    }
+	
+	
+	
+
+	
+	
+	
 }
--- Assets/Scripts/snowBallMove.cs
+++ Assets/Scripts/snowBallMove.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class snowBallMove : MonoBehaviour
 {
+	public GameObject particleEffectPrefab;
 	private Transform randomPosition;
 	private Transform TransformBall;
-	public float areaRadius = 2f;      // Radius of the pile
+	public float areaRadius = 10f;      // Radius of the pile
+public Transform target; // Drag the destination point here in the Inspector
+    public float forceAmount = .005f; // Set the force amount to control the speed
 
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +22,33 @@ public class snowBallMove : MonoBehaviour
                 Random.Range(-areaRadius, areaRadius));
 		TransformBall = GetComponent<Transform>();
 		TransformBall.position = randomPosition;
-				
+		rb = GetComponent<Rigidbody>();		
     }
+
+void FixedUpdate()
+    {
+        // Calculate the direction vector from the sphere to the target point
+        Vector3 direction = (target.position - transform.position).normalized;
+
+        // Apply force in that direction
+        rb.AddForce(direction * forceAmount);
+    }
+	
+	
+	void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("Target") || collision.gameObject.name.Contains("Tree")|| collision.game || Object.name.Contains("Spruce")collision.game||Object.name.Contains("Pine"))
+        {
+            // Instantiate the particle effect at the target's location
+            Instantiate(particleEffectPrefab, transform.position, Quaternion.identity);
+
+            // Destroy the target GameObject
+            Destroy(gameObject);
+        }
+		
+
+
+
 
     // Update is called once per frame
     void Update()
